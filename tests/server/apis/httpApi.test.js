b3aef259a2e486ecf7b460546ea3c0c0a3750364
Roberto Betini Junior <betini.roberto@gmail.com>
2026-05-18 01:51:59 -0300

    test: uncomment tests

--- tests/server/apis/httpApi.test.js
+++ tests/server/apis/httpApi.test.js
@@ -1,58 +1,58 @@
-// import { describe, it } from "node:test";
-// import assert from "node:assert";
+import { describe, it } from "node:test";
+import assert from "node:assert";
 
-// import httpApi from "../../../server/apis/httpApi.js";
+import httpApi from "../../../server/apis/httpApi.js";
 
-// const HTTP_PORT = 9090;
+const HTTP_PORT = 9090;
 
-// describe("HTTP Api", () => {
-//     it("create room should be ok", async (t) => {
-//         const server = httpApi.run(HTTP_PORT);
+describe("HTTP Api", () => {
+    it("create room should be ok", async (t) => {
+        const server = httpApi.run(HTTP_PORT);
 
-//         const url = `http://localhost:${HTTP_PORT}/rooms`;
-//         const response = await fetch(url, { method: "POST" });
-//         const roomId = await response.text();
+        const url = `http://localhost:${HTTP_PORT}/rooms`;
+        const response = await fetch(url, { method: "POST" });
+        const roomId = await response.text();
 
-//         assert.strictEqual(response.status, 201);
-//         assert.notEqual(roomId, null);
-//         assert.notEqual(roomId, "");
+        assert.strictEqual(response.status, 201);
+        assert.notEqual(roomId, null);
+        assert.notEqual(roomId, "");
 
-//         server.close();
-//     });
+        server.close();
+    });
 
-//     it("get home page should be ok", async (t) => {
-//         const server = httpApi.run(HTTP_PORT);
+    it("get home page should be ok", async (t) => {
+        const server = httpApi.run(HTTP_PORT);
 
-//         const url = `http://localhost:${HTTP_PORT}/`;
-//         const response = await fetch(url);
-//         const page = await response.text();
+        const url = `http://localhost:${HTTP_PORT}/`;
+        const response = await fetch(url);
+        const page = await response.text();
 
-//         assert.strictEqual(response.status, 200);
-//         assert.notEqual(page, null);
-//         assert.notEqual(page, "");
+        assert.strictEqual(response.status, 200);
+        assert.notEqual(page, null);
+        assert.notEqual(page, "");
         
-//         server.close();
-//     });
+        server.close();
+    });
 
-//     it("get room page should be ok", async (t) => {
-//         const server = httpApi.run(HTTP_PORT);
+    it("get room page should be ok", async (t) => {
+        const server = httpApi.run(HTTP_PORT);
 
-//         let url = `http://localhost:${HTTP_PORT}/rooms`;
-//         let response = await fetch(url, { method: "POST" });
+        let url = `http://localhost:${HTTP_PORT}/rooms`;
+        let response = await fetch(url, { method: "POST" });
 
-//         assert.strictEqual(response.status, 201);
+        assert.strictEqual(response.status, 201);
 
-//         const roomId = await response.text();
+        const roomId = await response.text();
 
-//         url = `http://localhost:${HTTP_PORT}/rooms/${roomId}`;
-//         response = await fetch(url);
-//         const page = await response.text();
+        url = `http://localhost:${HTTP_PORT}/rooms/${roomId}`;
+        response = await fetch(url);
+        const page = await response.text();
 
-//         assert.strictEqual(response.status, 200);
-//         assert.notEqual(page, null);
-//         assert.notEqual(page, "");
+        assert.strictEqual(response.status, 200);
+        assert.notEqual(page, null);
+        assert.notEqual(page, "");
         
-//         server.close();
-//     });
-// });
+        server.close();
+    });
+});
 
